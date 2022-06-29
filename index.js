82cf556aff2ec28b8907569f6a3cb9cfa7498859
Nitsuga <88418234+Agusst1n@users.noreply.github.com>
2022-06-28 23:43:59 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import chalk from 'chalk';
+import {suma, multiplica} from "./controller.js";
+
+
+
+
+console.log(chalk.green(suma(34,6), 'Multiplicacion'));
+
+
+console.log(chalk.green(multiplica(7,5), 'Multiplicacion'));
